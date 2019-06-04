a6e67927385f2128ba45a2b53ff6b9f51d5d088f
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-06-04 19:34:00 -0400

    Demo Components

--- client/src/js/demo/Header.js
+++ client/src/js/demo/Header.js
@@ -1,15 +1,31 @@
 import React, { Component} from 'react'
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import Home from './Home.js'
+import Pantry from './Pantry.js'
+import Suggestions from './Suggestions.js'
+import Shopping from './Shopping.js'
 
 export default class Header extends Component {
 
   render() {
     return (
-      <header>
-        <h1> Chexpiry </h1>
-        <h2> An Intellegent way to reduce waste </h2>
-      </header>
+      <Router>
+        <header>
+          <nav id="demo-nav">
+            <ul>
+              <li> <Link to="/demo/home"> Home </Link></li>
+              <li> <Link to="/demo/shopping"> Go Shopping </Link></li>
+              <li> <Link to="/demo/pantry"> The Pantry </Link></li>
+              <li> <Link to="/demo/suggestions"> My Suggestions </Link></li>
+            </ul>
+            <Route path="/demo/home" component={Home} />
+            <Route path="/demo/shopping" component={Shopping} />
+            <Route path="/demo/pantry" component={Pantry} />
+            <Route path="/demo/suggestions" component={Suggestions} />
+          </nav>
+        </header>
+      </Router>
     )
   }
+
 }
--- client/src/js/demo/Home.js
+++ client/src/js/demo/Home.js
@@ -4,14 +4,19 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div id="box">
-        <p> <strong> A MORSEL OF IMPACT </strong> </p>
-        <p> GET STARTED ON REDUCING FOOD WASTE </p>
-        <p>
-        Upload photos from your grocery shopping for information and reminders about expiration.
-        In time, you can expect suggestions about how you can improve your shopping habits to reduce
-        waste. Let's make sustainable choices, one bite at a time.
-        </p>
+      <div id="background">
+        <div id="container">
+          <div id="box">
+          <p className="center"> <strong> A MORSEL OF IMPACT </strong> </p>
+          <p className="section-heading"> GET STARTED ON REDUCING FOOD WASTE </p>
+          <p>
+          Upload photos from your grocery shopping for information and reminders about expiration.
+          In time, you can expect suggestions about how you can improve your shopping habits to reduce
+          waste. Let's make sustainable choices, one bite at a time.
+          </p>
+          </div>
+          <img src="img/intro.jpg" />
+        </div>
       </div>
     )
   }
