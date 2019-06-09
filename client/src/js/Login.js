c778b9f7635ac79e62e2b2dc893f090f0a6d9029
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-06-08 21:31:01 -0400

    Updating Main Page Components

--- client/src/js/Login.js
+++ client/src/js/Login.js
@@ -4,13 +4,27 @@ export default class Login extends Component {
 
   render() {
     return (
-      <div>
+      <div id="background">
         <h2> Login </h2>
-        <form method="POST">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="submit" />
-        </form>
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-4">
+                <div className="card">
+                    <div className="card-body">
+                        <form action="" autocomplete="off">
+                            <div className="form-group">
+                                <input type="text" className="form-control" name="username" placeholder="Email" />
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control" name="password" placeholder="Password" />
+                            </div>
+                            <button type="button" id="sendlogin" className="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- client/src/js/Register.js
+++ client/src/js/Register.js
@@ -4,16 +4,39 @@ export default class Register extends Component {
 
   render() {
     return (
-      <div>
+      <div id="background">
         <h2> Register </h2>
-        <form method="POST">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="text" placeholder="Phone Number" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="submit" />
-        </form>
+        <div className="container">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-4">
+                <div className="card">
+                    <div className="card-body">
+                        <form action="" autocomplete="off">
+                            <div className="form-group">
+                                <input type="text" className="form-control" name="first_name" placeholder="First Name" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control" name="last_name" placeholder="Last Name" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control" name="phone-number" placeholder="Phone Number" />
+                            </div>
+                            <div className="form-group">
+                                <input type="text" className="form-control" name="email" placeholder="Email" />
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control" name="password" placeholder="Password" />
+                            </div>
+                            <div className="form-group">
+                                <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" />
+                            </div>
+                            <button type="button" id="sendlogin" className="btn btn-primary">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
