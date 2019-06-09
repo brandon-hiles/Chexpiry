add5ce43fe5a8c5938a2bbbccd38849b995a2f50
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-06-08 21:31:43 -0400

    Updating Pantry Component

--- client/src/js/auth/Pantry.js
+++ client/src/js/auth/Pantry.js
@@ -2,9 +2,53 @@ import React, {Component} from 'react'
 
 export default class Pantry extends Component {
 
+
+
   render() {
     return (
-      <h2> Pantry Page </h2>
+      <div id="pantry">
+        <h2> Pantry Page </h2>
+        <table className="table">
+          <thead>
+            <th> Produce Item </th>
+            <th> Date Entered </th>
+            <th> Expiry Date </th>
+            <th> Delete Item </th>
+          </thead>
+          <tbody>
+            <tr>
+              <td> Item 1 </td>
+              <td> 06/08/2019 </td>
+              <td> 06/10/2019 </td>
+              <td> <input type="submit" /> </td>
+            </tr>
+            <tr>
+              <td> Item 2 </td>
+              <td> 06/08/2019 </td>
+              <td> 06/12/2019 </td>
+              <td> <input type="submit" /> </td>
+            </tr>
+            <tr>
+              <td> Item 3 </td>
+              <td> 06/09/2019 </td>
+              <td> 06/15/2019 </td>
+              <td> <input type="submit" /> </td>
+            </tr>
+            <tr>
+              <td> Item 4 </td>
+              <td> 06/07/2019 </td>
+              <td> 06/10/2019 </td>
+              <td> <input type="submit" /> </td>
+            </tr>
+            <tr>
+              <td> Item 5 </td>
+              <td> 06/08/2019 </td>
+              <td> 06/10/2019 </td>
+              <td> <input type="submit" /> </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
