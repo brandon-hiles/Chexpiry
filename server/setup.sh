ca58bd2711ebcd5b75191aaac22da4692ab583f3
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-06-11 22:34:27 -0400

    Updating flask env variables

---
+++ server/setup.sh
@@ -0,0 +1,7 @@
+echo "Starting up virtualenv"
+source env/bin/activate
+pip3 install flask
+pip3 install python-dotenv
+pip3 install SQLAlchemy
+pip3 install tensorflow
+pip3 install boto # AWS SDK for python
