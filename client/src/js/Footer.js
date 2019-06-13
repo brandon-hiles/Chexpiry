58c8979366e05845f336cb0ebd6bcb8e7c0b89da
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-06-12 23:08:28 -0400

    Updating Footer Component

--- client/src/js/Footer.js
+++ client/src/js/Footer.js
@@ -4,8 +4,21 @@ export default class Footer extends Component {
 
   render() {
     return (
-      <footer>
-       <p className='footer'> Footer section </p>
+      <footer id="footer">
+        <div className="footer-top">
+
+        </div>
+        <div className="footer-bottom">
+          <div className="icons-container">
+            <p className="socials"> Follow me on the socials </p>
+            <a target="_blank" href="#"> <img src="img/icons/footer-icon-github.svg" /> </a>
+            <a target="_blank" href="#"> <img src="img/icons/footer-icon-twitter.svg" /> </a>
+            <a target="_blank" href="#"> <img src="img/icons/footer-icon-linkedin.svg" /> </a>
+          </div>
+          <div className="copyright-section">
+            <p className="copyright"> &copy; Copyright 2019 Chexpiry. All rights are reversed</p>
+          </div>
+        </div>
       </footer>
     )
   }
