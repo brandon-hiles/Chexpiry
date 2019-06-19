ae10cc161e2f545c8bad70b9b5415b9c64eff347
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-06-18 21:54:11 -0400

    Updating Index Component

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -4,20 +4,24 @@ export default class Index extends Component {
 
     render() {
       return (
-        <div id="background">
-          <div>
-            <div id="box">
-            <p className="center"> <strong> A MORSEL OF IMPACT </strong> </p>
-            <p className="section-heading"> GET STARTED ON REDUCING FOOD WASTE </p>
-            <p>
-            Upload photos from your grocery shopping for information and reminders about expiration.
-            In time, you can expect suggestions about how you can improve your shopping habits to reduce
-            waste. Let's make sustainable choices, one bite at a time.
-            </p>
-            </div>
-            <img src="img/intro.jpg" />
+        <section id="home">
+            <div id="background">
+            <div className="container">
+              <div id="box">
+                <h2>
+                  <span className="section-heading-upper"> A morsel of impact </span>
+                  <span className="section-heading-lower"> Get started on reducing food waste </span>
+                </h2>
+                  <p>
+                  Upload photos from your grocery shopping for information and reminders about expiration.
+                  In time, you can expect suggestions about how you can improve your shopping habits to reduce
+                  waste. Let's make sustainable choices, one bite at a time.
+                  </p>
+              </div>
+            <img src="img/intro.jpg" className="intro-img"/>
           </div>
         </div>
+      </section>
       )
   }
 }
