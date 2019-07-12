26c3216cdf8804bd09caf2ad94e5799cf9bb1c69
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-11 22:41:35 -0400

    Updating api routes

--- server/app/api/v1/api_route.py
+++ server/app/api/v1/api_route.py
@@ -1,18 +1,51 @@
 from flask import jsonify,json
+from flask import request
+from sqlalchemy import exc
+
 
 from app.api.v1 import api
-from app.api.v1.src.login import Login
 from app.api.v1.src.aws import AWS
+from app.api.v1.src.user import User_Object
 
-# User variables
-first_name = "Brandon"
-last_name = "Hiles"
-phone = "416-579-0452"
-email = None
-password = "password"
+# Database Endpoint
 
-# Login object
-login = Login(first_name=first_name, last_name=last_name, phone=phone, email=email, password=password)
+# Create User
+@api.route('/db/users/store', methods=['POST'])
+def store():
+
+    # Initialize our user requested paramters
+    first_name = request.args.get('first_name')
+    last_name = request.args.get("last_name")
+    phone = request.args.get("phone")
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    # Initialize our User object
+    user = User_Object(first_name=first_name,
+                last_name=last_name,
+                phone=phone,
+                email=email,
+                password=password)
+    try:
+        user.store()
+        return jsonify({'Status' : 200, 'Message' : 'User has been successfully added'})
+    except exc.SQLAlchemyError:
+        return jsonify({'Status' : 404, 'Message' : 'User has already been added'})
+
+# Users
+@api.route('/db/<database>/user/store', methods=['POST'])
+def store_user(database):
+
+    # Initialize our user requested paramters
+    first_name = request.args.get('first_name')
+    last_name = request.args.get("last_name")
+    phone = request.args.get("phone")
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    # Define Our User object
+
+    return f"User Object ({first_name} {last_name} {phone} {email} {password})"
 
 # User Endpoint
 @api.route('/user/check', methods=['GET'])
@@ -25,24 +58,19 @@ def get_users():
 
 @api.route('/user', methods=['POST'])
 def create_user():
-    pass
+    return "Create User"
 
 @api.route('/user', methods=['PUT'])
 def update_user(id):
-    pass
+    return "Update User"
 
 # Recipe Endpoint
 @api.route('/recipes', methods=['GET'])
 def get_recipes():
     return "Recipe List"
 
-# Pantry/Suggestions Endpoint
-@api.route('/user/<int: id>/pantry/', methods=['GET'])
-def get_pantry(id):
-    return 'Pantry List'
-
 # AWS Endpoint
-@api.route('/aws')
+@api.route('/aws', methods=['GET', 'PUT'])
 def connect():
     aws.upload(file="README.md", user='brandon')
     return render_template('aws.html')
