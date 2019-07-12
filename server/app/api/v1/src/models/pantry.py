8b66dcca92f507b36a5a541f2f9fd5df77b5b524
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-11 22:40:48 -0400

    Removing old files

--- server/app/api/v1/src/models/pantry.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Pantry(db.Model):
-
-    user_id = db.Column(db.Integer, primary_key=True)
-    item = db.Column(db.String(64))
-    created_time = db.Column(db.String(100))
-    expiry_time = db.Column(db.String(100))
-    delete = db.Column(db.Boolean)
--- server/app/src/api/ai/prepend.py
+++
@@ -1,7 +0,0 @@
-
-
-class Validation_Data(object):
-
-    def __init__(self, urls):
-        self.urls = urls
-        
