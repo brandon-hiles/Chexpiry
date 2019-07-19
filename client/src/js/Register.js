f6c6ebb5f082131d64dfbc667b99ed2edd7be679
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-18 21:17:34 -0400

    Add API Calls

--- client/src/js/Register.js
+++ client/src/js/Register.js
@@ -2,37 +2,97 @@ import React, { Component} from 'react'
 
 export default class Register extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      first_name: "",
+      last_name: "",
+      phone_number: "",
+      email: "",
+      password: "",
+      confirm_password: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(evt) {
+    // do something with form data
+    evt.preventDefault();
+    alert(`You typed: ${this.state.email}`);
+    this.setState({email : ""});
+  }
+
+  handleChange(evt) {
+    // runs on every keystroke event
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
   render() {
     return (
       <div id="background">
-        <h2> Register </h2>
         <div className="container">
           <div className="row justify-content-center align-items-center">
             <div className="col-4">
                 <div className="card">
                     <div className="card-body">
                       <h3 id="login-header"> Register </h3>
-                        <form action="localhost:5000/api/v1/login/create" autoComplete="off">
+                        <form onSubmit={this.handleSubmit}>
                             <div className="form-group">
-                                <input type="text" className="form-control" name="first_name" placeholder="First Name" />
+                                <input type="text"
+                                className="form-control"
+                                name="first_name"
+                                placeholder="First Name"
+                                value={this.state.first_name}
+                                onChange={this.handleChange}
+                                />
                             </div>
                             <div className="form-group">
-                                <input type="text" className="form-control" name="last_name" placeholder="Last Name" />
+                                <input type="text"
+                                className="form-control"
+                                name="last_name"
+                                placeholder="Last Name"
+                                value={this.state.last_name}
+                                onChange={this.handleChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" className="form-control" name="phone-number" placeholder="Phone Number" />
+                                <input type="text"
+                                className="form-control"
+                                name="phone_number"
+                                placeholder="Phone Number"
+                                value={this.state.phone_number}
+                                onChange={this.handleChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" className="form-control" name="email" placeholder="Email" />
+                                <input type="text"
+                                className="form-control"
+                                name="email"
+                                placeholder="Email"
+                                value={this.state.email}
+                                onChange={this.handleChange} />
                             </div>
                             <div className="form-group">
-                                <input type="password" className="form-control" name="password" placeholder="Password" />
+                                <input type="password"
+                                className="form-control"
+                                name="password"
+                                placeholder="Password"
+                                value={this.state.password}
+                                onChange = {this.handleChange} />
                             </div>
                             <div className="form-group">
-                                <input type="password" className="form-control" name="confirm_password" placeholder="Confirm Password" />
+                                <input type="password"
+                                className="form-control"
+                                name="confirm_password"
+                                placeholder="Confirm Password"
+                                value={this.state.confirm_password}
+                                onChange= {this.handleChange}/>
                             </div>
                             <p id="login-button">
-                              <button type="button" id="sendlogin" className="btn btn-primary btn-color">Register</button>
+                              <button type="button"
+                              id="sendlogin"
+                              className="btn btn-primary btn-color">Submit</button>
                             </p>
                         </form>
                     </div>
