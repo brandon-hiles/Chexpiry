202a275a96aaddeefdf198afc3e230adf0c74587
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-07-22 14:01:18 -0400

    Updating our models file

--- server/app/models.py
+++ server/app/models.py
@@ -4,8 +4,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(35), index=True)
     last_name = db.Column(db.String(40), index=True)
-    email = db.Column(db.String(120), index=True, unique=True)
     phone = db.Column(db.String(20), index=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128), index=True)
 
     def __repr__(self):
