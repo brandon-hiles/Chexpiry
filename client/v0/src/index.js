fb7cd788d482698a4e5af9ce3bfbcf596f1e817d
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-06-11 21:56:42 -0400

    Updating index.js

--- client/v0/src/index.js
+++ client/v0/src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import Pantry from './components/auth/Pantry';
+//ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Pantry />, document.getElementById('root'));
